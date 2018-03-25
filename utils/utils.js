4e2cf63bb5d41f4ac5ae3fd05542f3f23df1980c
Francis Santos <frabbo@hotmail.com>
2018-03-25 19:53:08 +0200

    Test

--- utils/utils.js
+++ utils/utils.js
@@ -1,4 +1,5 @@
 function getCookie(cookies, name) {
+    console.log(cookies);
     var value = "; " + cookies;
     var parts = value.split("; " + name + "=");
     if (parts.length == 2) return parts.pop().split(";").shift();
