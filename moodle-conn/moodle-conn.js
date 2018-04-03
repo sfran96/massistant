7caa39c4adc82e977d12bb0028b64bd0e9969ab9
Francis Santos <frabbo@hotmail.com>
2018-04-03 12:07:41 +0200

    mysql remove fix

--- moodle-conn/moodle-conn.js
+++ moodle-conn/moodle-conn.js
@@ -14,14 +14,12 @@ var connection = mysql.createConnection({
  * @param {function(string)} callback 
  */
 function IsUserLoggedIn(moodCookValue, callback) {
-    connection.connect();
     connection.query("SELECT * FROM `mdl_sessions` WHERE `sid` LIKE '" + moodCookValue + "'", (error, results, fields) => {
         // Si hay resultados
         if (results != undefined && results.length > 0 && results[0].userid !== 0) {
             callback(results[0].userid);
         }
     });
-    connection.end();
 }
 
 module.exports = {
