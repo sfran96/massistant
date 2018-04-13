da8737434075e214669ffa51ee458c419c62f254
Francis Santos <frabbo@hotmail.com>
2018-04-13 21:28:27 +0200

    course "global" option

--- menus.js
+++ menus.js
@@ -2,25 +2,26 @@
 const conf = require('./conf.json');// Opciones del submenú de la asignatura
 
 var courseMenu = [
-    { name: "Abrir", type: "bullet" },
-    { name: "Buscar (beta)", type: "bullet" },
-    { name: "Navegar", type: "bullet" },
-    { name: "Calificaciones", type: "bullet" },
-    { name: "Profesores", type: "bullet" },
-    { name: "Menú global", type: "bullet" }
+    { name: "Abrir", option: "open", type: "bullet" },
+    { name: "Buscar (beta)", option: "search", type: "bullet" },
+    { name: "Navegar", option: "navigate", type: "bullet" },
+    { name: "Calificaciones", option: "califications", type: "bullet" },
+    { name: "Profesores", option: "teachers", type: "bullet" },
+    { name: "Menú global", option: "global", type: "bullet" }
 ]
 // Opciones del submenú de los mensajes
 var messageMenu = [
     { name: "Abrir" },
     { name: "Responder" },
     { name: "Redactar" },
-    { name: "Eliminar" }
+    { name: "Eliminar" },
+    { name: "Menú global", option: "global", type: "bullet" }
 ]
 // Opciones del menú global
 var globalMenu = [
     { name: "Página principal", url: `${conf.self.host}/my`, type: "home" },
-    { name: "Asignaturas", url: undefined, type: "course", menu: courseMenu },
-    { name: "Mensajes", url: `${conf.self.host}/message/index.php`, type: "message", menu: messageMenu },
+    { name: "Asignaturas", url: undefined, type: "course" },
+    { name: "Mensajes", url: `${conf.self.host}/message/index.php`, type: "message" },
     { name: "Calificaciones", url: `${conf.self.host}/grade/report/overview/index.php`, type: "clasification" },
     { name: "Desplegar menú", url: undefined, type: "menu_toggle" },
     { name: "Presentación", url: undefined, type: "config" },
