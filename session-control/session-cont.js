7d79845d63e625719c7bb5ba4043a82cf5be94d0
Francis Santos <frabbo@hotmail.com>
2018-04-19 20:44:15 +0200

    sending user host info through socket

--- session-control/session-cont.js
+++ session-control/session-cont.js
@@ -2,6 +2,7 @@
  * Define el módulo encargado de la comprobación de sesión del usuario
  */
 const moodleConn = require('../moodle-conn/moodle-conn');
+const conf = require('../conf.json');
 const utils = require('../utils/utils');
 
 /**
@@ -13,19 +14,29 @@ function checkUserStatus(socket, next) {
     // Comprobamos que tiene la cookie iniciada
     var cookieMoodle = utils.getCookie("MoodleSession", socket.request.headers.cookie);
     // Si la cookie está definida, y no se ha registrado su sesión en el sistema de sockets, buscar en la base de datos SQL
-    if ((cookieMoodle != "" || cookieMoodle != undefined) && socket.handshake.session.cookieMoodle != cookieMoodle) {
-        moodleConn.isUserLoggedIn(cookieMoodle, (userId) => {
-            socket.handshake.session.userId = userId;
-            socket.handshake.session.cookieMoodle = cookieMoodle;
-            socket.join(userId);
+    if ((cookieMoodle != "" || cookieMoodle != undefined)) {
+        if (socket.handshake.session.cookieMoodle != cookieMoodle) {
+            moodleConn.isUserLoggedIn(cookieMoodle, (userId) => {
+                socket.handshake.session.userId = userId;
+                socket.handshake.session.cookieMoodle = cookieMoodle;
+                _whenLoggedIn(socket);
+                next();
+            });
+            // Si SÍ está definida, coger los datos de la sesión
+        } else if (socket.handshake.session.cookieMoodle === cookieMoodle) {
+            _whenLoggedIn(socket);
             next();
-        });
-        // Si SÍ está definida, coger los datos de la sesión
-    } else if ((cookieMoodle != "" || cookieMoodle != undefined) && socket.handshake.session.cookieMoodle === cookieMoodle) {
-        socket.join(socket.handshake.session.userId);
-        socket.emit('joined', {});
-        next();
+        }
     }
+}
+
+/**
+ * 
+ * @param {SocketIO.Socket} socket Socket del usuario que solicita el servicio
+ */
+function _whenLoggedIn(socket) {
+    socket.join(socket.handshake.session.userId);
+    socket.emit('hostInfo', conf.self.host);
 }
 
 module.exports = {
